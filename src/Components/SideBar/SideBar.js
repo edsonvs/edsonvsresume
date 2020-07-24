48c02c6c45dd2b0401a4fa2e16267070c15fdc95
edsonvs <43698796+edsonvs@users.noreply.github.com>
2020-07-25 03:39:23 +0800

    Update SideBar.js

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -12,7 +12,7 @@ const SideDrawer = props => {
             <ul>
                 <li><a href="/">About</a></li>
                 <li><a href="/">Skills</a></li>
-                <li><a href="/">Projects</a></li>
+                <li><a href="/">Education</a></li>
                 <li><a href="/">Experience</a></li>
             </ul>
         </nav>
